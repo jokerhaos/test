797dcbd2ea68b284a9730340f5195f86e4c8f0d4
joker <1214096242@qq.com>
2022-07-20 16:43:06 +0800

    1

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"test/models"
 	"test/routers"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// 1.创建路由
+	// 创建路由
 	r := gin.Default()
 	routers.InitRouter(r)
+	// 创建数据库链接
+	fmt.Println("创建数据库连接")
+	models.ConnectDB()
+
 	// 2.监听端口，默认在8080
 	r.Run(":8000")
 }
